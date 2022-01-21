93a9ef60c0b6812f1b97ba91c991ce624a0943a5
sarah-bruschi <sarah.bruschi@gmail.com>
2022-01-21 08:53:27 -0500

    created QuizBrain class with a:question_number, question_list m:next_question which takes the users answer to the question being asked from the question bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -9,3 +10,5 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+quiz.next_question()
